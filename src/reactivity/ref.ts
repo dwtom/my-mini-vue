dbc003632ff9eb9305166d9fa0d9eaaecb5cc2b6
dwtom <1119086531@qq.com>
2023-07-14 10:57:35 +0800

    feat: 实现isRef,unRef以及proxyRef

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -8,6 +8,7 @@ class RefImpl {
   private _value: any;
   public dep; // 对应唯一的属性-value
   private _rawValue: any;
+  public __v_isRef = true;
   constructor(value) {
     this._rawValue = value; // 保存原始值
     // 如果是对象则用reactive包裹，简单数据类型直接保存
@@ -45,4 +46,33 @@ function convert(value) {
 
 export function ref(value) {
   return new RefImpl(value);
+}
+
+export function isRef(ref: any) {
+  // 如果是ref则有内部属性，不是ref则原本应该是undefined
+  return Boolean(ref.__v_isRef);
+}
+
+export function unRef(ref: any) {
+  // 是ref-返回其value
+  // 不是ref-直接返回
+  return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRefs(objWithRefs) {
+  return new Proxy(objWithRefs, {
+    get(target, key) {
+      // is ref-> ref.value
+      // not ref -> original value
+      return unRef(Reflect.get(target, key));
+    },
+    set(target, key, value) {
+      // 普通类型的值会同时改变原本ref的值
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    },
+  });
 }
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from '../effect';
-import { ref } from '../ref';
+import { reactive } from '../reactive';
+import { isRef, proxyRefs, ref, unRef } from '../ref';
 
 describe('ref', () => {
   // ref基本结构
@@ -39,5 +40,48 @@ describe('ref', () => {
     expect(dummy).toBe(1);
     a.value.count = 2;
     expect(dummy).toBe(2);
+  });
+
+  // 实现isRef和unRef
+  it('isRef', () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 10,
+    });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
+  });
+
+  it('unRef', () => {
+    const a = ref(1);
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
+  });
+
+  // 实现proxyRefs
+  // proxyRefs包裹的ref可以在使用时省略.value
+  it('proxyRefs', () => {
+    const user = {
+      age: ref(10),
+      name: 'zhangsan',
+    };
+    const proxyUser = proxyRefs(user);
+    // proxyRefs的get
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    // proxyRefs对象内的普通属性直接返回
+    expect(proxyUser.name).toBe('zhangsan');
+
+    // set
+    // proxyRefs是引用类型，set会改变原本的ref
+    proxyUser.age = 20;
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
+
+    // 如果赋值是ref类型则直接替换
+    proxyUser.age = ref(10);
+    expect(proxyUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
   });
 });
