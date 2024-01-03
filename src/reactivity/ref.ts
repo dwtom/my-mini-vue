401266dd073f60237f1e2e7b1204bd5003978c46
dwtom <1119086531@qq.com>
2024-01-03 15:21:18 +0800

    feat: 实现proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -53,3 +53,22 @@ export function isRef(ref) {
 export function unRef(ref) {
   return isRef(ref) ? ref.value : ref;
 }
+
+// 使用ref时省略.value 用于template
+// 可传入ref对象或者普通的值
+// 因为proxyRefs引用了源对象，所以新对象的属性值改变会引起源对象的属性值改变
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    // ref返回.value内的值或者普通对象直接返回属性值
+    get(target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    },
+  });
+}
