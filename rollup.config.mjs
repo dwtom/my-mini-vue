3bd2eba6158c8734dee32d0d87ec12636ec9b771
dwtom <1119086531@qq.com>
2024-01-04 11:16:05 +0800

    chore: 安装rollup生态并初步完成打包配置

---
+++ rollup.config.mjs
@@ -0,0 +1,18 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      // esm
+      format: 'es',
+      file: 'lib/my-mini-vue.esm.js',
+    },
+    {
+      // commonjs
+      format: 'cjs',
+      file: 'lib/my-mini-vue.cjs.js',
+    },
+  ],
+  plugins: [typescript()],
+};
